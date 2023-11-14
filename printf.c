cefe6d6f380e2f25d468577b23e3b5f8b28cbe3b
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-11-14 11:13:56 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -21,15 +21,8 @@ int _printf(const char *format, ...)
             case 's':
                 count += write_string(args);
                 break;
-            case 'd':
-            case 'i':
-                count += write_integer(args);
-                break;
             case '%':
-                count += write_percent(args);
-                break;
-            case 'b':  // New case for %b
-                count += write_binary(args);
+                count += write_percent();
                 break;
             }
         }
@@ -56,19 +49,7 @@ int write_string(va_list args)
     return write(1, va_arg(args, char *), 1);
 }
 
-int write_integer(va_list args)
-{
-    /* Implement integer printing logic */
-    return 0;
-}
-
-int write_percent(va_list args)
+int write_percent(void)
 {
     return write(1, "%", 1);
-}
-
-int write_binary(va_list args)
-{
-    /* Implement binary printing logic for %b */
-    return 0;
 }
