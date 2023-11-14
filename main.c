2b9123430e190e588473bfabae1a23bd731fe293
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-11-14 11:11:48 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, %s! This is a number: %d\n", "World", 42);
+    _printf("Binary representation of %d: %b\n", 42, 42);  // Using %b
+
+    return 0;
+}
